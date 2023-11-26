f286dc2ffba80409ce259c96a7c5e03cd4349b35
shakhbozbekusmonov <miracle_0319@bk.ru>
2023-11-26 12:43:34 +0500

    Customize Radix UI Theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,15 @@
 import "@radix-ui/themes/styles.css";
+import "./theme-config.css";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Theme } from "@radix-ui/themes";
 import Navbar from "@/components/Navbar";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ 
+  subsets: ["latin"], 
+  variable: "--font-inter" 
+});
 
 export const metadata: Metadata = {
   title: "Issue Tracker",
@@ -19,10 +23,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
-      <body className={inter.className}>
-        <Theme>
+      <body className={inter.variable}>
+        <Theme appearance='light' accentColor='violet'>
           <Navbar />
-          <main className="p-5">{children}</main>
+          <main className='p-5'>{children}</main>
         </Theme>
       </body>
     </html>
